24e92278cbd0b519cfed8115a15a6bb15fc1acdf
tadeh <tadehb83@gmail.com>
2021-05-25 11:52:18 +0430

    Add CatalogContext class

---
+++ MicroServicesApp/src/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Data.Interfaces;
+using Catalog.Api.Entities;
+using Catalog.Api.Settings;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(ICatalogDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            Products = database.GetCollection<Product>(settings.CollectionName);
+        }
+    }
+}

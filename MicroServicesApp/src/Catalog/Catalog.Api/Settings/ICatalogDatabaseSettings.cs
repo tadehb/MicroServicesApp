968be61c4715bfdd3da0804ffc9902761880895e
tadeh <tadehb83@gmail.com>
2021-05-25 11:37:13 +0430

    Add Catalog Database Settings

---
+++ MicroServicesApp/src/Catalog/Catalog.Api/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.Api.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
--- MicroServicesApp/src/Catalog/Catalog.Api/Startup.cs
+++ MicroServicesApp/src/Catalog/Catalog.Api/Startup.cs
@@ -1,15 +1,11 @@
+using Catalog.Api.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.Api
 {
@@ -27,6 +23,8 @@ namespace Catalog.Api
         {
 
             services.AddControllers();
+            services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+            services.AddSingleton<ICatalogDatabaseSettings>(sp => sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.Api", Version = "v1" });

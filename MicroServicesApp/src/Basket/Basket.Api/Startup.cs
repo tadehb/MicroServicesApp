35c59cb0db35155bffecaa876b30b9ab1205505c
tadeh <tadehb83@gmail.com>
2021-05-26 12:16:50 +0430

    Add Redis Configuration

--- MicroServicesApp/src/Basket/Basket.Api/Startup.cs
+++ MicroServicesApp/src/Basket/Basket.Api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace Basket.Api
 {
@@ -25,7 +20,13 @@ namespace Basket.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(sp =>
+            {
 
+                var configuration = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ MicroServicesApp/src/Basket/Basket.Api/data/BasketContext.cs
@@ -0,0 +1,18 @@
+﻿using Basket.Api.data.Interfaces;
+using StackExchange.Redis;
+
+namespace Basket.Api.data
+{
+    public class BasketContext : IBasketContext
+    {
+        public IDatabase Redis { get; }
+
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = redisConnection.GetDatabase();
+        }
+    }
+}

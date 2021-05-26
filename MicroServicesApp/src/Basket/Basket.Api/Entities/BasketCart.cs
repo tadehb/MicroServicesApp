699f39da5a400ba4190381ca96a5921c662f7a9c
tadeh <tadehb83@gmail.com>
2021-05-26 11:33:53 +0430

    Add Basket Entity

---
+++ MicroServicesApp/src/Basket/Basket.Api/Entities/BasketCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class BasketCart
+    {
+        public string UserName { get; set; }
+        public List<BasketItems> Items { get; set; } = new List<BasketItems>();
+
+        public BasketCart()
+        {
+
+        }
+        public BasketCart(string userName) => UserName = UserName;
+
+        public decimal TotalPrice
+        {
+
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
